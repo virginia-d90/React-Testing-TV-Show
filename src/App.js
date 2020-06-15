770cf7c68b5a79126ff4c5a14f872f1e0ba6abd2
Virginia Davenport <virginialdport@gmail.com>
2020-06-15 15:21:16 -0600

    moved async call, first test in Episodes.spec is working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
+import { fetchShow } from "./api/fetchShow"
 
 import Episodes from "./components/Episodes";
 import "./styles.css";
@@ -14,19 +15,29 @@ export default function App() {
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
 
+  // useEffect(() => {
+  //   const fetchShow = () => {
+  //     axios
+  //       .get(
+  //         "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+  //       )
+  //       .then(res => {
+  //         console.log(res)
+  //         setShow(res.data);
+  //         setSeasons(formatSeasons(res.data._embedded.episodes));
+  //       });
+  //   };
+  //   fetchShow();
+  // }, []);
+
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
-  }, []);
+    fetchShow()
+      .then(res => {
+        console.log(res.data._embedded.episodes);
+        setShow(res.data);
+        setSeasons(formatSeasons(res.data._embedded.episodes));
+      });
+  }, [])
 
   const handleSelect = e => {
     setSelectedSeason(e.value);
